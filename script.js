c9de2d8ce33a5c4c1f87285ba8272a11462514d1
Sharas <30733669+asmx3r@users.noreply.github.com>
2018-06-22 08:10:18 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,7 @@
-/*
-$(document).ready(function) {
-   $('tr').hide().filter(function() {
-      return $(this).find('td.atviras').length;
-   }).addClass('atidarytas').css('display', 'table-row').click(function() {
-      $(this).nextUntil('tr.atidarytas').css('display', function(i, v) {
-         return this.style.display === 'table-row' ? 'none' : 'table-row';
-      });
-   });
-}
-*/
-$(document).ready(function() {
-   $('[data-toggle="toggle"]').change(function() {
-      $(this).parents().next('.atviras').toggle();
-   });
+$( document ).ready(function() {
+
+	//collapse and expand sections
+	$('#tableMain').on('click', 'tr.breakrow',function(){
+	$(this).nextUntil('tr.breakrow').slideToggle(200);
+	
 });
